be8392268386a03ea4c54fc813ef775dc1549252
Marik-D <maretskii@gmail.com>
2018-02-13 23:27:37 +0200

    Add console logging to service

--- ArkWatch.MonitorService/Monitor.cs
+++ ArkWatch.MonitorService/Monitor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Timers;
@@ -32,29 +33,37 @@ namespace ArkWatch.MonitorService
             if(recordRunning) return;
             recordRunning = true;
 
-            var data = _storage.LoadData();
-            var history = _historyStorage.LoadData();
-
-            foreach (var server in data.Servers)
+            try
             {
-                Console.WriteLine($"[{DateTime.Now}] Requesting {server.Address}");
+                var data = _storage.LoadData();
+                var history = _historyStorage.LoadData();
 
-                var info = ServerQuery.ServerQuery.Query(server.GetIpEndPoint()).Result;
-                history.FindOrCreate(server.Address).Records.Add(new HistoryRecord(DateTime.Now, info.Players.Select(p => p.Name)));
-
-                var newPlayers = info.Players.Where(player => data.Players.All(p => p.Name != player.Name))
-                    .Select(player => new Player(player.Name, ""));
-
-                Console.WriteLine($"[{DateTime.Now}] Record {server.Address}: {info.Players.Count} players");
-
-                foreach (var player in newPlayers)
+                foreach (var server in data.Servers)
                 {
-                    data.Players.Add(player);
+                    Console.WriteLine($"[{DateTime.Now}] Requesting {server.Address}");
+
+                    var info = ServerQuery.ServerQuery.Query(server.GetIpEndPoint()).Result;
+                    history.FindOrCreate(server.Address).Records
+                        .Add(new HistoryRecord(DateTime.Now, info.Players.Select(p => p.Name)));
+
+                    var newPlayers = info.Players.Where(player => data.Players.All(p => p.Name != player.Name))
+                        .Select(player => new Player(player.Name, ""));
+
+                    Console.WriteLine($"[{DateTime.Now}] Record {server.Address}: {info.Players.Count} players");
+
+                    foreach (var player in newPlayers)
+                    {
+                        data.Players.Add(player);
+                    }
                 }
+
+                _storage.SaveData(data);
+                _historyStorage.SaveData(history);
             }
-            
-            _storage.SaveData(data);
-            _historyStorage.SaveData(history);
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
             recordRunning = false;
         }
@@ -62,11 +71,16 @@ namespace ArkWatch.MonitorService
         public void Start()
         {
             _timer.Start();
+
+            Console.WriteLine("Service started");
+            Console.WriteLine($"Working dir: {Directory.GetCurrentDirectory()}");
+            Console.WriteLine($"Record interval: {Interval}");
         }
 
         public void Stop()
         {
             _timer.Stop();
+            Console.WriteLine("Service stopped");
         }
 
         public TimeSpan Interval
